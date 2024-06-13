a264e8d4f9d52eacfc58d9c4dee27536f9f83e77
Sankavi-D <69241322+Sankavi-D@users.noreply.github.com>
2024-06-13 13:46:16 +0530

    tests added to register user

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json())
+
 app.post('/users', (req, res) => {
-  res.json({ status_code: 200, message: 'success' })
+  const { username, password } = req.body
+  if (!password || !username) {
+    res.sendStatus(400)
+    return
+  }
+
+  res.json({ status_code: 200, message: 'user registration successful' })
 });
 
 module.exports = app;
--- app.test.js
+++ app.test.js
@@ -5,18 +5,31 @@ describe('POST /users', () => {
   describe('username and password given', () => {
     test('respond with status code 200', async () => {
       const response = await request(app).post('/users').send({
-        username: "sankavi",
-        password: 'sankavi@123'
+        username: "username",
+        password: 'password'
       })
-      expect(response.statusCode).toBe(200);
+      expect(response.statusCode).toBe(200)
     })
+  })
+  test("should specify json in the content type header", async () => {
+    const response = await request(app).post("/users").send({
+      username: "username",
+      password: "password"
+    })
+    expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
   })
 
   describe('username and password not given', () => {
-    // test('respond with 404 error', () => {
-    //   const response = await request(app).post('/users').send({
-    //     user
-    //   })
-    // })
+    test("respond with 400 error", async () => {
+      const bodyData = [
+        {username: "username"},
+        {password: "password"},
+        {}
+      ]
+      for (const body of bodyData) {
+        const response = await request(app).post("/users").send(body)
+        expect(response.statusCode).toBe(400)
+      }
+    })
   })
 })
